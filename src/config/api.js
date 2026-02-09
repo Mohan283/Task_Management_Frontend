50c24914bc72f778dd44ce51770231e694818408
Mohan283 <Mohan91328@gmail.com>
2026-02-09 13:49:58 +0530

    api change

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-   baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
-  withCredentials: true
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: false, // ✅ MUST be false
 });
 
 export default API;
